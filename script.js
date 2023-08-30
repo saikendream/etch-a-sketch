8c26aacfe7acb4a4b7daf084cfc8c33887a3b224
Jae <121826115+saikendream@users.noreply.github.com>
2023-08-30 12:35:52 -0300

    Attempt at fixing the slider update

--- script.js
+++ script.js
@@ -3,16 +3,18 @@
 const slider = document.getElementById("canvasSize");
 const output = document.getElementById("sliderValue");
 output.innerHTML = slider.value;
+let currentSize = slider.value;
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
+    currentSize = this.value;
     output.innerHTML = this.value;
 };
 
 // Canvas Generator
 
 const canvas = document.querySelector("#canvas"); 
-for(let i=0; i < slider.value; i++) {
+for(let i=0; i < currentSize; i++) {
     const row = document.createElement('div');
     row.classList.add('row');
 
