5ac37d1a49f6213202db36e966fd10ecd2a32b59
Jae <121826115+saikendream@users.noreply.github.com>
2023-08-30 11:44:39 -0300

    Coded initial Canvas Generator

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Captures SLIDER value
+
 const slider = document.getElementById("canvasSize");
 const output = document.getElementById("sliderValue");
 output.innerHTML = slider.value;
@@ -6,4 +7,22 @@ output.innerHTML = slider.value;
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
-  };
+};
+
+// Canvas Generator
+
+const canvas = document.querySelector("#canvas"); 
+for(let i=0; i < slider.value; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+
+    canvas.appendChild(row);
+    for(let i=0; i < slider.value; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+    
+        row.appendChild(pixel);
+    }
+}
+
+// Buttons
